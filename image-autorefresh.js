4c7d4a6397794363e5339a1d68a653ef0eaac3be
kvu022 <kvu022@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-04 11:16:44 +0000

    Added 'alt' attribute to v 1.4
    git-svn-id: https://plugins.svn.wordpress.org/image-autorefresh-shortcode/tags/1.4@1082231 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- image-autorefresh.js
+++ image-autorefresh.js
@@ -4,15 +4,23 @@
 
 jQuery(document).ready(function(){
 
-
     /* set image width to the same as container */
-    jQuery('img.image_autorefresh').width(jQuery('#content').width());
+    jQuery('img.image_autorefresh')
+    		.not('.image_autorefresh_changed_width')
+    		.css('width',(jQuery('#content')
+    		.width()));
+
 
     /* when the window regains focus, reload all images */
     jQuery(document).focus(function(){
         jQuery('img.image_autorefresh').each(function(){
                d = new Date();
-               this.attr("src", this.data("src") + "?" + d.getTime());
+
+               if(this.data("query") == ''){
+            	   this.attr("src", this.data("src") + "?" + d.getTime());
+               }else{
+            	   this.attr("src", this.data("src") + "?" + d.getTime() + "&" + this.data("query"));
+               } 
         });
 
     });
--- image-autorefresh.php
+++ image-autorefresh.php
@@ -2,13 +2,12 @@
 /*
 Plugin Name: Image autorefresh shortcode
 Plugin URI: https://github.com/klasske/wp_image_autorefresh
-Description: Small shortcode plugin created specifically for reloading images for site.uit.no/spaceweather
-Version: 1.1
+Description: Ligthweight shortcode plugin created specifically for reloading images that are refreshed on a regular basis (for example from live camera feeds)
+Version: 1.4
 Author: Klaske van Vuurden
 Author URI: https://github.com/klasske/
 Copyright: Klaske van Vuurden
 */
-
 
 defined('ABSPATH') or die("Don't access directly!");
 
@@ -19,6 +18,11 @@ function image_autorefresh_shortcode($atts){
 			'refresh_time' => 60,
 			'caption' => '',
 			'align' => 'left',
+			'width' => 0,
+			'height' => 0,
+			'alt' => '',
+			'class' => '',
+			'query_string' => ''	
 	), $atts );
 
    /* second version: allows multiple images per page with random string */
@@ -33,16 +37,30 @@ function image_autorefresh_shortcode($atts){
             if(document.hasFocus()){
                d = new Date();
                image = jQuery('img#image_autorefresh_<?php echo $randomString ?>');
-               image.attr("src", image.data("src") + "?" + d.getTime());
+               if(image.data("query") == ''){
+	               image.attr("src", image.data("src") + "?" + d.getTime());
+               }else{
+	               image.attr("src", image.data("src") + "?" + d.getTime() + "&" + image.data("query"));
+               }    
             }
         }, <?php echo $a['refresh_time']; ?>000);
 
 	</script>
 
 	<div class="<?php echo '' != $a['caption'] ? 'wp-caption ': ''; ?>align<?php echo $a['align']; ?>">
-	<img src="<?php echo $a['src']; ?>" data-src="<?php echo $a['src']; ?>"
-	     class="image_autorefresh" id="image_autorefresh_<?php echo $randomString ?>"
-	     data-refresh="<?php echo $a['refresh_time']; ?>">
+	<img src="<?php echo $a['src']; echo ($a['query_string'] != '') ? '?' . $a['query_string'] : ''; ?>" 
+		 data-src="<?php echo $a['src']; ?>"
+	     data-query="<?php echo $a['query_string']; ?>"
+	     alt="<?php echo $a['alt']; ?>"	
+	     class="image_autorefresh<?php 
+	     echo ($a['width'] > 0) ? ' image_autorefresh_changed_width' : ''; 
+	     echo ($a['class'] != '') ? ' ' . $a['class'] : ''; 
+	     ?>" 
+	     id="image_autorefresh_<?php echo $randomString ?>"
+	     data-refresh="<?php echo $a['refresh_time']; ?>"
+	     <?php echo ($a['width'] > 0) ? 'width="' . $a['width'] . '"' : '' ?>
+	     <?php echo ($a['height'] > 0) ? 'height="' . $a['height'] . '"' : '' ?>
+	     >
 	<?php echo '' != $a['caption'] ? '<p class="wp-caption-text">' . $a['caption'] . '</p>' : ''; ?>
 	</div>
 	<?php
@@ -56,5 +74,4 @@ function image_autorefresh_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'image_autorefresh_scripts' );
 add_shortcode('image-autorefresh', 'image_autorefresh_shortcode');
-
 
